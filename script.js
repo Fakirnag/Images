6e9e4c03a59bb72b636a89ee039de9a1f1bc443b
deep ng <103912125+Fakirnag@users.noreply.github.com>
2024-08-30 23:47:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+
+		
+			
+function ChangeImg() {
+    
+    let cngimg = document.getElementById("cngimage");
+    cngimage.src = "panda.jpg";
+}
+
+function SeaImg(){
+    let Sea = document.getElementById("cngimage");
+ cngimage.src = "Sea.jpg";
+}
+
+function CoffeImg(){
+    let Coffe = document.getElementById("cngimage");
+    cngimage.src = "coffe.jpeg";
+}
+
+function TempleImg(){
+    let Temple =document.getElementById("cngimage");
+    cngimage.src ="temple.jpg";
+}
+    
+    
+function ShivLing(){
+    let Shivling = document.getElementById("cngimage");
+    cngimage.src = "shivling.jpeg";
+}
+
+function flower(){
+    let Flower = document.getElementById("cngimage");
+    cngimage.src = "leaves.jpeg";
+}
+
+
+function Leaves(){
+    let leaves = document.getElementById("cngimage");
+    cngimage.src = "leave.jpeg";
+}
+
+
+function Rose(){
+    let rose= document.getElementById("cngimage");
+    cngimage.src = "Rose.jpeg";
+}
+
+function daisies(){
+    let Daisies= document.getElementById("cngimage");
+    cngimage.src = "Flower.jpg";
+}
+
+
+function Road(){
+    let road = document.getElementById("cngimage");
+    cngimage.src="road.jpg";
+}
+
+
+function Jungle(){
+    let Jungle= document.getElementById("cngimage");
+    cngimage.src = "forest.jpg";
+}
+
+
+
+function Pasta(){
+    let pasta = document.getElementById("cngimage");
+    cngimage.src = "pasta.jpeg";
+}
+
+
+function pasta1(){
+    let Pasta1= document.getElementById("cngimage");
+    cngimage.src = "pasta1.jpeg";
+}
+
+
+function Manchurian(){
+    let manchurian = document.getElementById("cngimage");
+    cngimage.src = "manchurian.jpeg";
+}
+
+
+function Chowmin(){
+    let chowmin= document.getElementById("cngimage");
+    cngimage.src = "chowmin.jpeg";
+}
+
+
+function Daisies(){
+    let daisie= document.getElementById("cngimage");
+    cngimage.src = "daisies.jpeg";
+}
+
